488d85cb1085cf3364721b0e808296a7b2233a56
Aubri Henley <aubri.henley@gmail.com>
2021-06-28 18:01:47 -0400

    added eventListener to button and fixed game over message

--- assets/script.js
+++ assets/script.js
@@ -1,38 +1,50 @@
 // create a timer that starts when the start button is pushed, time should be subtracted from countdown when a wrong answer is selected
-var timeEl = document.querySelector("timerEl");
+var timerEl = document.querySelector("#time");
 
-var quizEl = document.getElementById("quizEl");
+var quizEl = document.getElementById("quiz");
 
-var timeLeft = 130;
+var message = 'Game Over';
 
-function setTime() {
+var timeLeft = 75;
+
+document.getElementById("startBtn").addEventListener("click", countDown)
+
+ function countDown(event){
+  event.preventDefault();
     //Sets interval in variable
   var timeInterval = setInterval(function() {
-      timeLeft--;
-      timerEl.textContent = timeLeft + "seconds";
 
-      if (timeLeft===0) {
-        //stops execution of action at set interval
-        clearInterval(timeInterval);
-        // sends game over message
-        // timeLeft.textContent=''
-        // displayMessage();
+    if (timeLeft > 1) {
+      timerEl.textContent = timeLeft + ' seconds';
+      //decrement time left by 1 
+      timeLeft--;
+    } else if(timeLeft===1) {
+      //changes seconds to second
+      timerEl.textContent = timeLeft + ' second';
+      timeLeft--;
+    } else {
+      timerEl.textContent = '';
+      //stops execution of action at set interval
+      clearInterval(timeInterval);
+      // sends game over message
+      displayMessage();
       }
   }, 1000);
 }
 
-console.log.(setTime());
-
-// //function to create game over message
+// function to create game over message
 function displayMessage() {
-    timeEl.textContent = "Game Over";
-    document.getElementById(".gameStatus").innerHTML ="Game Over";
+   if (timeLeft===0) {
+     clearInterval(timeLeft);
+     document.getElementById("gameStatus").innerHTML = "Game Over";
+    }
+  }
 
-    
-}
+
 
 // create a high scores button that shows the top 5 high scores of the game
 // create a quiz section that asks questions about javascript and is multiple choice 
 // when all questions are answered or time runs out dipslay game over and score
-// when game is over player can save initials and score
+// when game is over player can save initials and score: highscores container that is hidden and then can be displayed when button or link pushed
+
 
