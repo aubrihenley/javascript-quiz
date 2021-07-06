558b285f92c6f614f53d92431b219ec084c0d429
Aubri Henley <aubri.henley@gmail.com>
2021-07-06 19:45:26 -0400

    added questions array

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-// create a timer that starts when the start button is pushed, time should be subtracted from countdown when a wrong answer is selected
+//  Global variables and selectors
 var timerEl = document.querySelector("#time");
 
 var quizEl = document.getElementById("quiz");
@@ -7,9 +7,47 @@ var message = 'Game Over';
 
 var timeLeft = 75;
 
-var questions = {
 
-}
+
+// Quiz questions array
+let questions = [
+  {
+    questions:
+    options: [],
+    correct: 
+},
+{
+  questions: "Inside which HTML element do we put the JavaScript?"
+  options: ["<script>", "<js>", "<scripting>", "<javascript>"],
+  correct: 0
+},
+{
+  questions: "Where is the correct place to insert JavaScript?"
+  options: ["The <head> section", "The <body> section", "within a <div>", "After the footer"],
+  correct: 1
+},
+{
+  questions: "How do you write 'Hello World' in an alertr box?"
+  options: ["alertBox("Hello World");", "alert("Hello World");", "msgBox("Hello World");", "msg("Hello World");"],
+  correct: 
+},
+{
+  questions: "How do you create a function in JavaScript?"
+  options: ["function = myFunction()", "function:myFunction()", "function myFunction()", "create.function = myFunction"],
+  correct: 0
+},
+{
+  questions: "How do you call a function named 'myFunction'?"
+  options: ["call function myFunction()", "call myFunction()", "myFunction()", "myFunction(call)"],
+  correct: 2
+},
+{
+  questions: "How does a FOR Loop start"
+  options: ["for (i = 0; i <= 5)", "for (i <= 5; i++)", "for i = 1 to 5", "for (i = 0; i < 5; i++)"],
+  correct: 
+},
+
+// create a timer that starts when the start button is pushed, time should be subtracted from countdown when a wrong answer is selected
 const start = document.getElementById("startBtn")
 start.addEventListener("click", countDown)
 
